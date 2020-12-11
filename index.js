ba0643adf3768f9326c6649754cce9b40d9739b6
Jakeas <JakeASierra@gmail.com>
2020-12-11 16:51:51 -0500

    submitting again because it didn't update on codeGrader

--- index.js
+++ index.js
@@ -365,7 +365,7 @@ function randomize(/* Code here */){
 
 
  /* 💪💪💪💪💪💪 STRETCH 3: 💪💪💪💪💪💪
- Use advanced array methods (.map, .reduce, .filer) to refactor your MVP code (create an array of all artists born in the 1900s with .filter, for example) */
+ Use advanced array methods (.map, .reduce, .filer) to refactor your MVP code (create an array of all artists born in the 1900s with .filter) */
 
  
  
